cf00588cf20c328433e2f3cbd2fbdc8429450759
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-03 19:25:07 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,41 @@
-/**
-Iterate over promises serially.
+declare const pEachSeries: {
+	/**
+	Iterate over promises serially.
 
-@param input - Iterated over serially in the `iterator` function.
-@param iterator - Return value is ignored unless it's `Promise`, then it's awaited before continuing with the next iteration.
-@returns Fulfills when all promises in `input` and ones returned from `iterator` are fulfilled, or rejects if any of the promises reject. The fulfillment value is the original `input`.
-*/
-export default function pEachSeries<ValueType>(
-	input: Iterable<PromiseLike<ValueType> | ValueType>,
-	iterator: (element: ValueType, index: number) => unknown
-): Promise<ValueType[]>;
+	@param input - Iterated over serially in the `iterator` function.
+	@param iterator - Return value is ignored unless it's `Promise`, then it's awaited before continuing with the next iteration.
+	@returns Fulfills when all promises in `input` and ones returned from `iterator` are fulfilled, or rejects if any of the promises reject. The fulfillment value is the original `input`.
+
+	@example
+	```
+	import pEachSeries = require('p-each-series');
+
+	const keywords = [
+		getTopKeyword() //=> Promise
+		'rainbow',
+		'pony'
+	];
+
+	const iterator = async el => saveToDiskPromise(el);
+
+	(async () => {
+		console.log(await pEachSeries(keywords, iterator));
+		//=> ['unicorn', 'rainbow', 'pony']
+	})();
+	```
+	*/
+	<ValueType>(
+		input: Iterable<PromiseLike<ValueType> | ValueType>,
+		iterator: (element: ValueType, index: number) => unknown
+	): Promise<ValueType[]>;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function pEachSeries<ValueType>(
+	// 	input: Iterable<PromiseLike<ValueType> | ValueType>,
+	// 	iterator: (element: ValueType, index: number) => unknown
+	// ): Promise<ValueType[]>;
+	// export = pEachSeries;
+	default: typeof pEachSeries;
+};
+
+export = pEachSeries;
